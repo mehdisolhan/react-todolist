959607e6f9b940812e72c20635bf8b6d22af3c2d
mehdisolhan <mehdisolhan@gmail.com>
2020-07-04 15:36:31 +0300

    Updates

---
+++ precache-manifest.66f025153957ef3b1d6e198b1955c4f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ade984b7a08726caf55b22d4d63b0c2",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "8bdd519544887ae909cb",
+    "url": "/react-todolist/static/css/main.fed90256.chunk.css"
+  },
+  {
+    "revision": "c041900ac406b62c2c1f",
+    "url": "/react-todolist/static/js/2.6b54bb50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.6b54bb50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bdd519544887ae909cb",
+    "url": "/react-todolist/static/js/main.d780e267.chunk.js"
+  },
+  {
+    "revision": "dea162c310a378ff9d0d",
+    "url": "/react-todolist/static/js/runtime-main.82f4965a.js"
+  }
+]);
